ea4aba34434b1557d0327a4fc8202693ef47a23b
renttu <laamanen.mpi@gmail.com>
2014-08-27 16:01:23 +0300

    Adding match information sending for the clients

--- src/main/java/fi/uef/remotug/net/StartPacket.java
+++ src/main/java/fi/uef/remotug/net/StartPacket.java
@@ -2,13 +2,25 @@ package fi.uef.remotug.net;
 
 public class StartPacket extends BasePacket{
 	private long startTime;
+	private int matchDuration;
+	private int matchStartDelay;
 	
-	public StartPacket(long stime) {
-		this.startTime = stime;
+	public StartPacket(long startTime, int matchDuration, int matchStartDelay) {
+		this.startTime = startTime;
+		this.matchDuration = matchDuration;
+		this.matchStartDelay = matchStartDelay;
 	}
 	
 	public long getStartTime() {
 		return this.startTime;
+	}
+	
+	public int getMatchDuration() {
+		return this.matchDuration;
+	}
+	
+	public long getMatchStartDelay() {
+		return this.matchStartDelay;
 	}
 	
 	@Override
