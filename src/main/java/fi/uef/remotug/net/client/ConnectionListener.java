91054b0406a95eed20a56d91ec790a5f17f9ed4b
Mikko Laamanen <mikko@cslap103-2.local>
2014-08-27 12:12:38 +0300

    Implemented clientside message handling

--- src/main/java/fi/uef/remotug/net/client/ConnectionListener.java
+++ src/main/java/fi/uef/remotug/net/client/ConnectionListener.java
@@ -1,7 +1,10 @@
 package fi.uef.remotug.net.client;
 
+import java.util.HashMap;
+
 public interface ConnectionListener {
-	public void gameValuesChanged(float balance, float localForce, float remoteForce);
+	public void gameValuesChanged(float balance, HashMap<Integer, Float> balances);
 	public void readyAnnounced(int playerID);
 	public void winnerAnnounced(int winnerID);
+	public void startAnnounced(long serverTime);
 }
